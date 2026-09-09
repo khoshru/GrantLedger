70291e523bf5891e8a5bd83b95766539767e02c6
Dina Khoshrou <dinakhoshru.com>
2026-09-09 17:06:38 +0330

    feat: add email-based custom user model (accounts.User)

---
+++ accounts/models.py
@@ -0,0 +1,74 @@
+from typing import Any
+
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.core.exceptions import ValidationError
+from django.db import models
+
+
+class UserManager(BaseUserManager[AbstractBaseUser]):
+    """Manager where email is the unique identifier."""
+
+    use_in_migrations = True
+
+    def _create_user(
+        self,
+        email: str,
+        password: str | None,
+        **extra_fields: Any,
+    ) -> AbstractBaseUser:
+        if not email:
+            raise ValueError("An email address must be provided")
+        email = self.normalize_email(email).lower()
+        user = self.model(email=email, **extra_fields)  # type: ignore[misc]
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(
+        self, email: str, password: str | None = None, **extra_fields: Any
+    ) -> AbstractBaseUser:
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(
+        self, email: str, password: str | None = None, **extra_fields: Any
+    ) -> AbstractBaseUser:
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self._create_user(email, password, **extra_fields)
+
+
+class LowercaseEmailFields(models.EmailField):  # type: ignore[type-arg]
+    def clean(
+        self,
+        value: str | None,
+        model_instance: models.Model | None,
+    ) -> str | None:
+        value = super().clean(value, model_instance)
+        try:
+            return value.lower()
+
+        except AttributeError:
+            raise ValidationError("Enter a valid email address.") from None
+
+
+class User(AbstractUser):
+    username = None  # type: ignore[assignment]
+    email = LowercaseEmailFields("email address", unique=True)
+    first_name = models.CharField("first name", max_length=150, blank=True)
+    last_name = models.CharField("last name", max_length=150, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()  # type: ignore[assignment,misc]
+
+    def __str__(self) -> str:
+        return str(self.email)
